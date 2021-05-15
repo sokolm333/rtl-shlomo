d9a1c58eb8e331e9156abe763c424d886b7d5018
sokolm333 <sokolm333@gmail.com>
2021-05-15 11:11:09 +0300

    added section login

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
+	$('.login__prompt-btn').on('click', function () {
+		var max = 10000;
+		let rand = Math.round(Math.random() * max);
+		alert(`הקוד החדש שלך הוא ${rand}!`);
+	});
 
+	$('.login__btn--submit').on('click', function (event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var url = "some.html";
+		$(location).attr('href', url);
+	});
 });
