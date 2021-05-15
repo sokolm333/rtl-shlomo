e8b665ca050a3306c62c960f4a07ba6f8533c6f1
sokolm333 <sokolm333@gmail.com>
2021-05-15 20:35:52 +0300

    added login__btn--submit on click function

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,16 @@ $(function () {
 	$('.login__btn--submit').on('click', function (event) {
 		event.preventDefault();
 		event.stopPropagation();
-		var url = "some.html";
-		$(location).attr('href', url);
+		$('.login__input').each(function () {
+			if ($(this).val() === "") {
+				$(this).addClass('login__input--empty');
+			} else {
+				$(this).removeClass('login__input--empty');
+			}
+		});
+		if ($('.login__input').hasClass('login__input--empty')) { } else {
+			var url = "some.html";
+			$(location).attr('href', url);
+		}
 	});
 });
